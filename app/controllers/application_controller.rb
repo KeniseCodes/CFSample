8c2fa4b0a5c636c420222da1b1470a6a86ca805f
Kenise Scavella <keniset@gmail.com>
2015-04-22 20:00:25 -0400

    fixed user_mailer controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,11 +3,4 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  def thank_you
-	  @name = params[:name]
-	  @email = params[:email]
-	  @message = params[:message]
-	  UserMailer.contact_form(@email, @name, @message).deliver
-end
-
 end
--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -7,5 +7,12 @@ class UserMailer < ApplicationMailer::Base
 	  		:to => 'keniset@gmail.com', 
 	  		:subject => "A new contact form message from #{name}")
   end
-end
+
+  def thank_you
+	  @name = params[:name]
+	  @email = params[:email]
+	  @message = params[:message]
+	  UserMailer.contact_form(@email, @name, @message).deliver
+	end
+	
 end
