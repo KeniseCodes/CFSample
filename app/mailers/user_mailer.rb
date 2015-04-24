0983be543d7947722e580afb6e91a27bf32d6092
Kenise Scavella <keniset@gmail.com>
2015-04-23 20:46:07 -0400

    added thank you to static page controller

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -8,11 +8,4 @@ class UserMailer < ApplicationMailer::Base
 	  		:subject => "A new contact form message from #{name}")
   end
 
-  def thank_you
-	  @name = params[:name]
-	  @email = params[:email]
-	  @message = params[:message]
-	  UserMailer.contact_form(@email, @name, @message).deliver
-	end
-	
 end
