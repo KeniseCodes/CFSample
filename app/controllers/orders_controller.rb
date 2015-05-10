723556b07ee48d70f7809f75c26ef96860048e86
Kenise Scavella <keniset@gmail.com>
2015-05-10 18:31:47 -0400

    fixes contact form alignment

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,16 +1,24 @@
 class OrdersController < ApplicationController
-	before_filter :authenticate_user!
+before_action :set_order, only: [:show, :edit, :update, :destroy]
+before_filter :authenticate_user!
 	
 	def index 
+	
 	end
 
 	def show
+		@order = Order.find(params[:id])
 	end
 
 	def new
+		@order = Order.new
+	end
+
+	def edit
 	end
 
 	def create 
+		@order = Order.new(order_params)
 	end
 
 end
