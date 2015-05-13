45eb79cd0fe515f4f0ad49514772fc5acaf32b24
Kenise Scavella <keniset@gmail.com>
2015-05-12 20:57:53 -0400

    fixed comment controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,7 @@
 class CommentsController < ApplicationController
 	
 	def create
-		@product = Product.find(params [:product_id])
+		@product = Product.find(params[:product_id])
 		@comment = @product.comments.new(comment_params)
 		@comment.user = current_user
 		@comment.save
@@ -11,4 +11,9 @@ class CommentsController < ApplicationController
 	def destroy
 	end
 
+	private
+	def comment_params
+		params.require(:comment).permit(:body, :rating)
+	end
+	
 end
