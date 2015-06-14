5d5cb88baf6c8d73b7a88f81c9e2db350b175769
Kenise Scavella <keniset@gmail.com>
2015-06-13 20:27:54 -0400

    updated test with factory

--- spec/controllers/orders_controller_spec.rb
+++ spec/controllers/orders_controller_spec.rb
@@ -2,20 +2,18 @@ require 'rails_helper'
 
 describe OrdersController, :type => :controller do
   describe "GET #index" do
-    before :each do
-      request.env["devise.mapping"] = Devise.mappings[:user]
-     @user = User.create!(email: "me@home.com", password: "watching the telly")
-     
+    before do
+      @user = build(:user)
+      sign_in @user 
     end
+
     it "responds successfully with an HTTP 200 status code" do
-      sign_in @user
       get :index
       expect(response).to be_success
       expect(response).to have_http_status(200)
     end
 
     it "renders the index template" do
-      sign_in @user
       get :index
       expect(response).to render_template("index")
     end
--- spec/factories.rb
+++ spec/factories.rb
@@ -1,10 +1,18 @@
 FactoryGirl.define do
 	
 	factory :user do 
-		email "marie@france.com"
-		password "CakeCake"
-		admin false
+		first_name "Mama"
+    last_name "DooDoo"
+    email "me@home.com"
+    password "Some great password"
+    admin false
 	end
+
+	factory :admin, class: User do
+    first_name "Admin"
+    last_name "User"
+    admin true
+  end
 
 	factory :comment do
     body "This product is awesome!"
