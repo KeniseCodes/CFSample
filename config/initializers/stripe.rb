43a4ed2554e5105e8c43ec7165e883d99f237bc6
Kenise Scavella <keniset@gmail.com>
2015-08-03 21:43:23 -0400

    Commented out if/else stripe

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,13 +1,13 @@
-if Rails.env.production?
-  Rails.configuration.stripe = {
-    :publishable_key => ENV['STRIPE_PUBLISHABLE_KEY'],
-    :secret_key      => ENV['STRIPE_SECRET_KEY']
-  }
-else
+#if Rails.env.production?
+  #Rails.configuration.stripe = {
+   # :publishable_key => ENV['STRIPE_PUBLISHABLE_KEY'],
+   # :secret_key      => ENV['STRIPE_SECRET_KEY']
+ # }
+#else
   Rails.configuration.stripe = {
     :publishable_key => 'pk_test_U6okaKwsL3Rg5Naz0InKq5nE',
     :secret_key      => 'sk_test_52yFIF5cD8nHTMERVrc0pZqs'
   }
-end
+#end
 
 Stripe.api_key = Rails.configuration.stripe[:secret_key]
